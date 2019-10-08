65ef20197e5c1609dafde7c6ff859183a8dd6723
Dominc Carolan <dominiccarolan@gmail.com>
2019-10-08 21:50:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,3 +16,22 @@ class Dog
   end 
 end
 
+class Person
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end 
+
+  def job 
+    @job
+  end 
+  
+end
